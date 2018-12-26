75028663c6a2ca15f366da938ec09a667309d4c3
Brandon Berhent <bbedward@gmail.com>
2018-12-26 09:36:10 -0500

    Add flutter project

---
+++ ios/Classes/FlutterNanoCorePlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterNanoCorePlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/FlutterNanoCorePlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterNanoCorePlugin.h"
+
+@implementation FlutterNanoCorePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_nano_core"
+            binaryMessenger:[registrar messenger]];
+  FlutterNanoCorePlugin* instance = [[FlutterNanoCorePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
