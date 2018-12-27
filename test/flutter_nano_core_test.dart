6b506a0176935f5c88509751533405d8c8e976a8
Brandon Berhent <bbedward@gmail.com>
2018-12-27 13:20:42 -0500

    Add account creation test

--- test/flutter_nano_core_test.dart
+++ test/flutter_nano_core_test.dart
@@ -1,10 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:flutter_nano_core/NanoAccounts.dart';
+import 'package:flutter_nano_core/NanoAccountType.dart';
 import 'package:flutter_nano_core/NanoKeys.dart';
 
 void main() {
   test('test seed to private key', () {
     expect(NanoKeys.seedToPrivate('E11A48D701EA1F8A66A4EB587CDC8808D726FE75B325DF204F62CA2B43F9ADA1', 0), '67EDBC8F904091738DF33B4B6917261DB91DD9002D3985A7BA090345264A46C6');
     expect(NanoKeys.seedToPrivate('E11A48D701EA1F8A66A4EB587CDC8808D726FE75B325DF204F62CA2B43F9ADA1', 1), '22EDDBF0D72E9A4232C3FE6689A6CB0A228C9ED822715A63E2F8644AA2C905A4');
+  });
+
+  test('test address from seed', () {
+    var privKey = '67EDBC8F904091738DF33B4B6917261DB91DD9002D3985A7BA090345264A46C6';
+    expect(NanoAccounts.createAccount(NanoAccountType.BANANO, 
+                                      NanoKeys.createPublicKey(privKey)),
+          'ban_1p95xji1g5gou8auj8h6qcuezpdpcyoqmawao6mpwj4p44939oouoturkggc');
+    expect(NanoAccounts.createAccount(NanoAccountType.NANO, 
+                                      NanoKeys.createPublicKey(privKey)),
+          'nano_1p95xji1g5gou8auj8h6qcuezpdpcyoqmawao6mpwj4p44939oouoturkggc');
   });
 }
